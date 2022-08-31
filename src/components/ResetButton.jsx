25e2e5d220341e801d85f6a45503a07557589872
Anatolii Pyroh <tolikpirog7@gmail.com>
2022-08-31 17:27:38 +0300

    reset "gameOver" redux state on "new game" click

--- src/components/ResetButton.jsx
+++ src/components/ResetButton.jsx
@@ -2,29 +2,31 @@ import { Button } from "@mui/material";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { reset } from "../helpers/reset";
-import { updateGrid } from "../redux/reducers/gameDataSlice";
+import { updateGrid, updateIsGameOver } from "../redux/reducers/gameDataSlice";
 
 const ResetButton = () => {
-    const data = useSelector((state) => state.gameData.grid)
+  const data = useSelector((state) => state.gameData.grid);
   const dispatch = useDispatch();
   const resetGrid = () => {
-    dispatch(updateGrid(reset(data)))
-  }
+    dispatch(updateGrid(reset(data)));
+    dispatch(updateIsGameOver(false))
+  };
   return (
     <Button
-        onClick={resetGrid}
+      onClick={resetGrid}
       sx={{
         padding: "0.5rem",
         background: "#846f5b",
         color: "#f8f5f0",
-        width: "7rem",
+        width: "8rem",
         margin: "auto",
         marginBottom: "1rem",
         borderRadius: "0.5rem",
         fontWeight: "bold",
-        '&:hover': {
-            background: "#9e8976",
-        }
+        "&:hover": {
+          background: "#9e8976",
+        },
+        fontSize: "1rem",
       }}
     >
       New Game
